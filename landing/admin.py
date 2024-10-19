91aff7b6e80c1d823a96fe6103dc9d7ac6ad3f7b
NeuroTalos <neurotalos.26@yahoo.com>
2024-10-19 16:33:00 +0800

    updated

--- landing/admin.py
+++ landing/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Subscribers)
+class SubscriberAdmin(admin.ModelAdmin):
+    #list_display = ['name', 'email']
+    list_display = [field.name for field in Subscriber._meta.fields]
+    list_filter = ('name',)
+    search_fields = ['name', 'email']
+    #exclude = ["email"]
+    class Meta:
+        model = Subscriber
+
+admin.site.register(Subscriber, SubscriberAdmin)
